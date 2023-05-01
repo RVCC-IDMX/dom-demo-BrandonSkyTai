827dc57cf86b58878145d22419a79ebfd53704e6
BrandonSkyTai <bt325sky@gmail.com>
2023-04-30 23:51:52 -0400

    Added code

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-/* ATTENTION: THIS IS CODE FROM THE YOUTUBE CRASH COURSE. IT IS NOT MEANT TO RUN, IT IS JUST FOR LEARNING PURPOSES 
+//const ul = document.querySelector('.items');
 
-Video by Brad Traversy
-JavaScript Crash Course For Beginners
-https://youtu.be/hdI2bqOjy3c?t=4228 
+//ul.remove();
+//ul.lastElementChild.remove();
+//ul.firstElementChild.textContent = 'Hello';
+//ul.children[1].innerText = 'Brandon';
+//ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
 
-Start at 1:10:29
- */
+// const btn = document.querySelector('.btn');
+
+//btn.addEventListener('mouseout', (e) => {
+    // e.preventDefault();
+    // document.querySelector('#my-form').style.background = '#ccc';
+    // document.querySelector('body').classList.add('bg-dark');
+    // document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+    // });
+
+//btn.style.background = 'purple';
+
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myform.addEventListener('submit', onsubmit);
+
+function onsubmit(e) {
+  e.preventDefault();
+
+  if(nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    setTimeout(() => msg.remove(),3000);
+  } else {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`
+    ${nameInput.value} : ${emailInput.value}`));
+
+    userList.appendChild(li);
+    msg.innerHTML = 'Thank you!';
+    //clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
